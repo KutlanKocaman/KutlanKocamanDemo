5802f2d01a9f5676c32039868e4d75618d1ba500
Kutlan Kocaman <kutlankocaman@hotmail.co.uk>
2020-07-24 21:48:37 +0100

    Amend comment on failed login attempt lockout counter.

--- KutlanKocamanDemo/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ KutlanKocamanDemo/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -80,8 +80,7 @@ namespace KutlanKocamanDemo.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                //lockoutOnFailure = true in order to make failed login attempts increment the lockout counter.
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
