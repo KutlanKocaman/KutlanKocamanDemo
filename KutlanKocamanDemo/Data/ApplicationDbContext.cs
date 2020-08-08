a8738a0c67dda4e520407182735b63744ccc3112
Kutlan Kocaman <kutlankocaman@hotmail.co.uk>
2020-08-08 17:12:36 +0100

    Adding a customer model and views with basic CRUD operations.

--- KutlanKocamanDemo/Data/ApplicationDbContext.cs
+++ KutlanKocamanDemo/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using KutlanKocamanDemo.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,8 @@ namespace KutlanKocamanDemo.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ KutlanKocamanDemo/Data/Migrations/20200801150528_AddCustomerModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KutlanKocamanDemo.Data.Migrations
+{
+    public partial class AddCustomerModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 30, nullable: false),
+                    LastName = table.Column<string>(maxLength: 30, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    AddressLine1 = table.Column<string>(maxLength: 30, nullable: false),
+                    AddressLine2 = table.Column<string>(maxLength: 30, nullable: true),
+                    AddressLine3 = table.Column<string>(maxLength: 30, nullable: true),
+                    Locality = table.Column<string>(maxLength: 30, nullable: true),
+                    County = table.Column<string>(maxLength: 30, nullable: false),
+                    PostCode = table.Column<string>(maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.Sql("GRANT SELECT ON dbo.Customers TO [KKDemoRole]");
+            migrationBuilder.Sql("GRANT INSERT ON dbo.Customers TO [KKDemoRole]");
+            migrationBuilder.Sql("GRANT UPDATE ON dbo.Customers TO [KKDemoRole]");
+            migrationBuilder.Sql("GRANT DELETE ON dbo.Customers TO [KKDemoRole]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
