72a4b5b1124a0080b20f0263e05802daf45b0eb7
Kutlan Kocaman <kutlankocaman@hotmail.co.uk>
2020-09-10 21:09:37 +0100

    Getting 2FA working.

---
+++ KutlanKocamanDemo/Data/Migrations/20200910184619_Add2FAPermissions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KutlanKocamanDemo.Data.Migrations
+{
+    public partial class Add2FAPermissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("GRANT INSERT ON dbo.AspNetUserTokens TO [KKDemoRole]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("REVOKE INSERT ON dbo.AspNetUserTokens TO [KKDemoRole]");
+        }
+    }
+}
