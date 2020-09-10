14e065dfecb440747fd0653b13821b9f00a2f84a
Kutlan Kocaman <kutlankocaman@hotmail.co.uk>
2020-09-10 22:25:27 +0100

    Adding database permissions for Reset 2FA process.

---
+++ KutlanKocamanDemo/Data/Migrations/20200910212121_AddReset2FAPermissions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KutlanKocamanDemo.Data.Migrations
+{
+    public partial class AddReset2FAPermissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("GRANT UPDATE ON dbo.AspNetUserTokens TO [KKDemoRole]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("REVOKE UPDATE ON dbo.AspNetUserTokens TO [KKDemoRole]");
+        }
+    }
+}
