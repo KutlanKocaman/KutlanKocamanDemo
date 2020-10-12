f4576430afd76798be8a79de1e6e02c937294a77
Kutlan Kocaman <kutlankocaman@hotmail.co.uk>
2020-10-12 21:11:01 +0100

    Add support for Google Analytics for SPA.

--- KutlanKocamanDemo/wwwroot/js/spa.js
+++ KutlanKocamanDemo/wwwroot/js/spa.js
@@ -5,17 +5,22 @@ import { Route } from 'react-router';
 import { Layout } from './components/layout';
 import WordSearch from './components/algorithms/word-search.js';
 import { TopologicalSort } from './components/algorithms/topological-sort';
+import Analytics from 'react-router-ga';
 
 export default class App extends Component {
     static displayName = App.name;
 
     render() {
         return (
-            <Layout>
-                <Route exact path='/' component={WordSearch} />
-                <Route path='/TopologicalSort' component={TopologicalSort} />
-                <Route path='/WordSearch' component={WordSearch} />
-            </Layout>
+            <BrowserRouter basename='/SPA'>
+                <Analytics basename='/SPA' id='UA-179585462-1'>
+                    <Layout>
+                        <Route exact path='/' component={WordSearch} />
+                        <Route path='/TopologicalSort' component={TopologicalSort} />
+                        <Route path='/WordSearch' component={WordSearch} />
+                    </Layout>
+                </Analytics>
+            </BrowserRouter>
         );
     }
 }
@@ -23,7 +28,5 @@ export default class App extends Component {
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-    <BrowserRouter basename='/SPA'>
-        <App />
-    </BrowserRouter>,
+        <App />,
     rootElement);
